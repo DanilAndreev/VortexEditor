0651f0a8b7e8aa53f2fad8ada15a31b68d32daae
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2019-12-17 00:33:42 +0200

    added hash set (not finished)

--- OOP_Course_v001/HashSet.h
+++ OOP_Course_v001/HashSet.h
@@ -2,19 +2,35 @@
 #include "Set.h"
 
 template <class T>
-class HashSet : public Set<T> {
-public:
-	HashSet();
-	~HashSet();
+class HashSetNode {
+	T value;
+	size_t key;
+	HashSetNode(size_t key, T value) : value(value), key(key) {}
+	HashSetNode();
 };
 
 
 
-HashSet::HashSet()
-{
+template <class T>
+class HashSet : public Set<HashSetNode<T>> {
+public:
+	HashSet() : Set<HashSetNode<T>>(comparator) {}
+	~HashSet() {}
+	virtual void add(const T object);
+	virtual T get(const size_t hash) const;
+protected:
+	static bool comparator(HashSetNode<T> &less, HashSetNode<T> &higher) {
+		return less.key < higher.key;
+	}
+};
+
+template<class T>
+inline void HashSet<T>::add(const T object) {
+	Set<HashSetNode<T>>::add(HashSetNode<T>());
 }
 
-
-HashSet::~HashSet()
+template<class T>
+inline T HashSet<T>::get(const size_t hash) const
 {
+	return T();
 }
