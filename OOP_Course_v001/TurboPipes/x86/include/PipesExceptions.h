dfc691519fefb35197ac3903f642cc252f7445b1
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2019-12-22 16:26:11 +0200

    updated libraries to last versions

--- OOP_Course_v001/TurboPipes/x86/include/PipesExceptions.h
+++ OOP_Course_v001/TurboPipes/x86/include/PipesExceptions.h
@@ -1,5 +1,24 @@
 #pragma once
-/* Copyright 2019 Andrieiev Danil */
+/*
+   Pipeable.cpp, PipeableString.cpp PipeDispatcher.cpp, PipeDispatcherString.cpp Threadable.cpp
+   Pipeable.h PipeableString.h PipeDispatcher.h PipeDispatcherString.h PipesException.h Threadable.h TurboPipes.h
+   Copyright (C) 2019 Andrieiev Danil
+   This source code is provided 'as-is', without any express or implied
+   warranty. In no event will the author be held liable for any damages
+   arising from the use of this software.
+   Permission is granted to anyone to use this software for any purpose,
+   including commercial applications, and to alter it and redistribute it
+   freely, subject to the following restrictions:
+   1. The origin of this source code must not be misrepresented; you must not
+	  claim that you wrote the original source code. If you use this source code
+	  in a product, an acknowledgment in the product documentation would be
+	  appreciated but is not required.
+   2. Altered source versions must be plainly marked as such, and must not be
+	  misrepresented as being the original source code.
+   3. This notice may not be removed or altered from any source distribution.
+   Andrieiev Danil danssg08@gmail.com
+   version 1.1.2
+*/
 #include <exception>
 
 
@@ -7,42 +26,42 @@ namespace TurboPipes {
 	using namespace std;
 
 	class PipeException : public exception {
-		const char* what() const override { return "Error: PipeException"; }
+		const char* what() const noexcept override { return "Error: PipeException"; }
 	};
 
 	class PipeCreationException : public PipeException {
-		const char* what() const override { return "Error: PipeCreationException"; }
+		const char* what() const noexcept override { return "Error: PipeCreationException"; }
 	};
 
 	class PipeConnectionException : public PipeException {
-		const char* what() const override { return "Error: PipeConnectionException"; }
+		const char* what() const noexcept override { return "Error: PipeConnectionException"; }
 	};
 
 	class PipeWritingException : public PipeException {
-		const char* what() const override { return "Error: PipeWritingException"; }
+		const char* what() const noexcept override { return "Error: PipeWritingException"; }
 	};
 
 	class PipeNoDataException : public PipeConnectionException {
-		const char* what() const override { return "Error: PipeNoDataException"; }
+		const char* what() const noexcept override { return "Error: PipeNoDataException"; }
 	};
 
 	class PipeConnectedException : public PipeConnectionException {
-		const char* what() const override { return "Error: PipeConnectedException"; }
+		const char* what() const noexcept override { return "Error: PipeConnectedException"; }
 	};
 
 	class PipeListeningException : public PipeConnectionException {
-		const char* what() const override { return "Error: PipeListeningException"; }
+		const char* what() const noexcept override { return "Error: PipeListeningException"; }
 	};
 
 	class PipeCallNotImplementedException : public PipeConnectionException {
-		const char* what() const override { return "Error: PipeCallNotImplementedException"; }
+		const char* what() const noexcept override { return "Error: PipeCallNotImplementedException"; }
 	};
 
 	class PipeReadingException : public PipeConnectionException {
-		const char* what() const override { return "Error: PipeReadingException"; }
+		const char* what() const noexcept override { return "Error: PipeReadingException"; }
 	};
 
 	class PipeLostConnectionException : public PipeConnectionException {
-		const char* what() const override { return "Error: PipeLostConnectionException"; }
+		const char* what() const noexcept override { return "Error: PipeLostConnectionException"; }
 	};
 }
--- OOP_Course_v001_console_front/MagicJSON/x64/include/JsonTokenizer.h
+++ OOP_Course_v001_console_front/MagicJSON/x64/include/JsonTokenizer.h
@@ -1,13 +1,33 @@
 #pragma once
-#include "Object.h"
+/*
+   JsonArray.cpp, JsonObject.cpp JsonString.cpp, JsonTokenizer.cpp
+   JsonArray.h JsonExceptions.h JsonItem.h JsonObject.h JsonString.h JsonTokenizer.h MagicJSO.h
+   Copyright (C) 2019 Andrieiev Danil
+   This source code is provided 'as-is', without any express or implied
+   warranty. In no event will the author be held liable for any damages
+   arising from the use of this software.
+   Permission is granted to anyone to use this software for any purpose,
+   including commercial applications, and to alter it and redistribute it
+   freely, subject to the following restrictions:
+   1. The origin of this source code must not be misrepresented; you must not
+	  claim that you wrote the original source code. If you use this source code
+	  in a product, an acknowledgment in the product documentation would be
+	  appreciated but is not required.
+   2. Altered source versions must be plainly marked as such, and must not be
+	  misrepresented as being the original source code.
+   3. This notice may not be removed or altered from any source distribution.
+   Andrieiev Danil danssg08@gmail.com
+   version 1.0.4
+*/
 #include <string>
 #include <utility>
 
-using namespace std;
 
 namespace MagicJSON {
+	using namespace std;
+
 	typedef pair<size_t, wstring> Token;
-	class JsonTokenizer : public Object {
+	class JsonTokenizer {
 	public:
 		static const size_t EOF_TOKEN = 0;
 		static const size_t STRING_TOKEN = 1;
